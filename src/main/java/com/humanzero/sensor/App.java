e1d1f19b2e06babed328e0016dcb275c9ce3e4c9
Andrey Burmagin <humanzer0@debian>
2019-10-13 19:56:06 +0000

    implemented package collecting into list

--- src/main/java/com/humanzero/sensor/App.java
+++ src/main/java/com/humanzero/sensor/App.java
@@ -1,15 +1,21 @@
 package com.humanzero.sensor;
 
-import org.pcap4j.core.*;
+import org.pcap4j.core.NotOpenException;
+import org.pcap4j.core.PcapHandle;
+import org.pcap4j.core.PcapNativeException;
+import org.pcap4j.core.PcapNetworkInterface;
 import org.pcap4j.core.PcapNetworkInterface.PromiscuousMode;
 import org.pcap4j.packet.Packet;
+import org.pcap4j.util.NifSelector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.EOFException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
+import java.util.stream.Stream;
 
 /**
  * Ca
@@ -18,82 +24,45 @@ import java.util.concurrent.TimeoutException;
 
 public class App {
 
-//	private static void capturePackages(){
+	private static Stream<Packet> packetStream = Stream.empty();
+
+//	private static void capturePackets(){
 //		Will be implemented later
 //	}
 
-	private static List<PcapNetworkInterface> getNetworkInterfaceList() throws IOException{
+	public static void main(String[] args) {
 
-		List<PcapNetworkInterface> networkInterfaceList = null;
+		final Logger logger = LoggerFactory.getLogger(App.class);
+
+		PcapNetworkInterface networkInterface = null;
 
 		try {
-			networkInterfaceList = Pcaps.findAllDevs();
-		} catch (PcapNativeException e){
+			networkInterface = new NifSelector().selectNetworkInterface();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-		if (networkInterfaceList == null || networkInterfaceList.isEmpty()){
-			throw new IOException("No interfaces to capture");
+		int snapshotLength = 65536;
+		int timeout = 10;
+
+		List<Packet> packetList = new ArrayList<>();
+
+		try {
+		        PcapHandle packageHandler = networkInterface.openLive(snapshotLength, PromiscuousMode.PROMISCUOUS, timeout);
+
+		        while (true) {
+		            Packet packet = packageHandler.getNextPacketEx();
+		            logger.info(packageHandler.getTimestamp().toString() + " Header is: " +
+                            packet.getHeader() + " Payload is: " +
+                            packet.getPayload());
+		            packetList.add(packet);
+
+                    logger.info("Captured: " + packetList.size() + " packets." );
+                }
+
+		} catch (PcapNativeException | EOFException | TimeoutException | NotOpenException e) {
+				e.printStackTrace();
 		}
 
-		return networkInterfaceList;
 	}
-
-    public static void main( String[] args ){
-
-			final Logger logger = LoggerFactory.getLogger(App.class);
-
-			List<PcapNetworkInterface> networkInterfaceList = null;
-
-			try {
-				networkInterfaceList = App.getNetworkInterfaceList();
-			} catch (IOException e){
-				e.printStackTrace();
-			}
-
-			int snapLen = 65536;
-			PromiscuousMode mode = PromiscuousMode.PROMISCUOUS;
-			int timeout = 10;
-
-	    	for (PcapNetworkInterface networkInterface :
-					networkInterfaceList
-					){
-
-	    				new Thread(()-> {
-
-	    					PcapHandle packageHandler = null;
-
-							try {
-								packageHandler = networkInterface.openLive(snapLen, mode, timeout);
-							} catch (PcapNativeException e) {
-								e.printStackTrace();
-							}
-
-							Packet packet = null;
-							try {
-								if (packageHandler != null) {
-									packet = packageHandler.getNextPacketEx();
-								}
-							} catch (PcapNativeException | TimeoutException | NotOpenException | EOFException e) {
-								e.printStackTrace();
-							}
-
-							if (packageHandler != null) {
-								if (packet != null) {
-									logger.info(packageHandler.getTimestamp() +
-											" Header is: " +
-											packet.getHeader() +
-											" : " +
-											"Length is: " +
-											packet.length());
-								}
-							}
-
-							if (packageHandler != null) {
-								packageHandler.close();
-							}
-
-						}).start();
-				}
-    }
 }
