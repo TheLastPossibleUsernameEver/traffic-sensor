0cfc53d6cf33160fa299c427ba3ef66369bcfd47
Andrey Burmagin <humanzer0@debian>
2019-10-13 10:43:35 +0000

    Added capturing one package from each network interface

--- src/main/java/com/humanzero/sensor/App.java
+++ src/main/java/com/humanzero/sensor/App.java
@@ -1,44 +1,99 @@
 package com.humanzero.sensor;
 
-import java.io.IOException;
-import org.pcap4j.core.PcapNetworkInterface;
+import org.pcap4j.core.*;
 import org.pcap4j.core.PcapNetworkInterface.PromiscuousMode;
-import java.util.List;
-import org.pcap4j.core.Pcaps;
-import org.pcap4j.core.PcapHandle;
-import org.pcap4j.core.PcapNativeException;
 import org.pcap4j.packet.Packet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.EOFException;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeoutException;
+
 /**
- * Captures one package from each network interface
+ * Ca
  *
  */
+
 public class App {
 
-    public static void main( String[] args ) throws PcapNativeException{
+//	private static void capturePackages(){
+//		Will be implemented later
+//	}
 
-	//private static final Logger logger = LoggerFactory.getLogger(App.class); will be implemented later
+	private static List<PcapNetworkInterface> getNetworkInterfaceList() throws IOException{
 
-        List<PcapNetworkInterface> networkInterfaceList = Pcaps.findAllDevs();
+		List<PcapNetworkInterface> networkInterfaceList = null;
 
-	int snapLen = 65536;
-	PromiscuousMode mode = PromiscuousMode.PROMISCUOUS;
-	int timeout = 10;
-
-	    for (PcapNetworkInterface networkInterface : 
-	    		networkInterfaceList){
-			   PcapHandle handle = networkInterface.openLive(snapLen, mode, timeout);
-				
-			   Packet packet = handle.getNextPacketEx();
-			   //will be implemented with logger
-			   System.out.println("Header is: " + 
-					       packet.getHeader() + 
-					       " : " + 
-					       "Length is" + 
-					       packet.length());
-			   handle.close();
+		try {
+			networkInterfaceList = Pcaps.findAllDevs();
+		} catch (PcapNativeException e){
+			e.printStackTrace();
 		}
+
+		if (networkInterfaceList == null || networkInterfaceList.isEmpty()){
+			throw new IOException("No interfaces to capture");
+		}
+
+		return networkInterfaceList;
+	}
+
+    public static void main( String[] args ){
+
+			final Logger logger = LoggerFactory.getLogger(App.class);
+
+			List<PcapNetworkInterface> networkInterfaceList = null;
+
+			try {
+				networkInterfaceList = App.getNetworkInterfaceList();
+			} catch (IOException e){
+				e.printStackTrace();
+			}
+
+			int snapLen = 65536;
+			PromiscuousMode mode = PromiscuousMode.PROMISCUOUS;
+			int timeout = 10;
+
+	    	for (PcapNetworkInterface networkInterface :
+					networkInterfaceList
+					){
+
+	    				new Thread(()-> {
+
+	    					PcapHandle packageHandler = null;
+
+							try {
+								packageHandler = networkInterface.openLive(snapLen, mode, timeout);
+							} catch (PcapNativeException e) {
+								e.printStackTrace();
+							}
+
+							Packet packet = null;
+							try {
+								if (packageHandler != null) {
+									packet = packageHandler.getNextPacketEx();
+								}
+							} catch (PcapNativeException | TimeoutException | NotOpenException | EOFException e) {
+								e.printStackTrace();
+							}
+
+							if (packageHandler != null) {
+								if (packet != null) {
+									logger.info(packageHandler.getTimestamp() +
+											" Header is: " +
+											packet.getHeader() +
+											" : " +
+											"Length is: " +
+											packet.length());
+								}
+							}
+
+							if (packageHandler != null) {
+								packageHandler.close();
+							}
+
+						}).start();
+				}
     }
 }
