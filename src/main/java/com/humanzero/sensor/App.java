6919d3b9a64487901d0b2448d9f7af677c22ce7e
root <root@humanzer0>
2019-10-13 03:04:38 +0300

    Added pcap4j and slf4j dependencies. Added capturing one packet from each interface

--- src/main/java/com/humanzero/sensor/App.java
+++ src/main/java/com/humanzero/sensor/App.java
@@ -1,13 +1,44 @@
 package com.humanzero.sensor;
 
+import java.io.IOException;
+import org.pcap4j.core.PcapNetworkInterface;
+import org.pcap4j.core.PcapNetworkInterface.PromiscuousMode;
+import java.util.List;
+import org.pcap4j.core.Pcaps;
+import org.pcap4j.core.PcapHandle;
+import org.pcap4j.core.PcapNativeException;
+import org.pcap4j.packet.Packet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
- * Hello world!
+ * Captures one package from each network interface
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main( String[] args ) throws PcapNativeException{
+
+	//private static final Logger logger = LoggerFactory.getLogger(App.class); will be implemented later
+
+        List<PcapNetworkInterface> networkInterfaceList = Pcaps.findAllDevs();
+
+	int snapLen = 65536;
+	PromiscuousMode mode = PromiscuousMode.PROMISCUOUS;
+	int timeout = 10;
+
+	    for (PcapNetworkInterface networkInterface : 
+	    		networkInterfaceList){
+			   PcapHandle handle = networkInterface.openLive(snapLen, mode, timeout);
+				
+			   Packet packet = handle.getNextPacketEx();
+			   //will be implemented with logger
+			   System.out.println("Header is: " + 
+					       packet.getHeader() + 
+					       " : " + 
+					       "Length is" + 
+					       packet.length());
+			   handle.close();
+		}
     }
 }
