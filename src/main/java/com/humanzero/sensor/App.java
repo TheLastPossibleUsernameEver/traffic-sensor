31697b0cdd820682e4f04d6461fcca5d3f3dc8a1
Andrey Burmagin <humanzer0@debian>
2019-10-13 23:19:12 +0000

    Added spark-core and spark-streaming dependencies, initialized streaming context

--- src/main/java/com/humanzero/sensor/App.java
+++ src/main/java/com/humanzero/sensor/App.java
@@ -1,8 +1,9 @@
 package com.humanzero.sensor;
 
-import org.pcap4j.core.NotOpenException;
+import org.apache.spark.SparkConf;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.pcap4j.core.PcapHandle;
-import org.pcap4j.core.PcapNativeException;
 import org.pcap4j.core.PcapNetworkInterface;
 import org.pcap4j.core.PcapNetworkInterface.PromiscuousMode;
 import org.pcap4j.packet.Packet;
@@ -10,11 +11,9 @@ import org.pcap4j.util.NifSelector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.EOFException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeoutException;
 
 /**
  * Ca
@@ -29,11 +28,38 @@ public class App {
 
 	private static List<Packet> packetList = new ArrayList<>();
 
-	//	private static void capturePackets(){
-//		Will be implemented later
-//	}
+	private static void collectPackets(){
+		try {
+			int snapshotLength = 65536;
+			int timeout = 10;
+			PcapHandle packageHandler = networkInterface.openLive(snapshotLength, PromiscuousMode.PROMISCUOUS, timeout);
+
+			new Thread(()->{
+				try {
+					while (true) {
+						Packet packet = packageHandler.getNextPacketEx();
+						System.out.println(packageHandler.getTimestamp().toString() + " Header is: " +
+								packet.getHeader() + " Payload is: " +
+								packet.getPayload());
+						packetList.add(packet);
+
+						logger.info("Captured: " + packetList.size() + " packets." );
+					}
+				} catch (Exception e) {
+
+				}
+
+			}).start();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 	public static void main(String[] args) {
+
+		SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("traffic-sensor");
+		JavaStreamingContext javaStreamingContext = new JavaStreamingContext(conf, Durations.seconds(1));
 
 		try {
 			networkInterface = new NifSelector().selectNetworkInterface();
@@ -41,24 +67,7 @@ public class App {
 			e.printStackTrace();
 		}
 
-		try {
-			int snapshotLength = 65536;
-			int timeout = 10;
-			PcapHandle packageHandler = networkInterface.openLive(snapshotLength, PromiscuousMode.PROMISCUOUS, timeout);
-
-		        while (true) {
-		            Packet packet = packageHandler.getNextPacketEx();
-		            logger.info(packageHandler.getTimestamp().toString() + " Header is: " +
-                            packet.getHeader() + " Payload is: " +
-                            packet.getPayload());
-		            packetList.add(packet);
-
-                    logger.info("Captured: " + packetList.size() + " packets." );
-                }
-
-		} catch (PcapNativeException | EOFException | TimeoutException | NotOpenException e) {
-				e.printStackTrace();
-		}
+		collectPackets();
 
 	}
 }
