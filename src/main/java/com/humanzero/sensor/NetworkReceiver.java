ddddbe1cc5ab2b53f1a2775ff0c4553cbc3f3c46
Andrey Burmagin <humanzer0@debian>
2019-11-18 19:02:10 -0100

    refact: extracted openPacketHandler method, removed getInterfaceAny method, moved exception handling into onStart method

--- src/main/java/com/humanzero/sensor/NetworkReceiver.java
+++ src/main/java/com/humanzero/sensor/NetworkReceiver.java
@@ -2,11 +2,12 @@ package com.humanzero.sensor;
 
 import org.apache.spark.storage.StorageLevel;
 import org.apache.spark.streaming.receiver.Receiver;
-import org.pcap4j.core.PcapHandle;
-import org.pcap4j.core.PcapNativeException;
-import org.pcap4j.core.PcapNetworkInterface;
-import org.pcap4j.core.Pcaps;
+import org.pcap4j.core.*;
 import org.pcap4j.packet.Packet;
+
+import java.io.EOFException;
+import java.util.Objects;
+import java.util.concurrent.TimeoutException;
 
 /**
  * Custom Receiver that receives the data from all network interfaces
@@ -14,51 +15,41 @@ import org.pcap4j.packet.Packet;
 
 public class NetworkReceiver extends Receiver<Packet> {
 
-    private static PcapNetworkInterface getInterfaceAny(){
-
-        PcapNetworkInterface networkInterfaceAny = null;
-
-        try {
-            networkInterfaceAny = Pcaps.getDevByName("any");
-        } catch (PcapNativeException e) {
-            e.printStackTrace();
-        }
-
-        return networkInterfaceAny;
-    }
-
     NetworkReceiver(){
         super(StorageLevel.MEMORY_AND_DISK_2());
     }
 
     @Override
     public void onStart() {
-        new Thread(this::receive).start();
+        new Thread(() -> {
+            try {
+                receive();
+            } catch (PcapNativeException | EOFException | TimeoutException | NotOpenException e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 
     @Override
     public void onStop() {
-
     }
 
-    private void receive(){
+    private static PcapHandle openPacketHandler(PcapNetworkInterface networkInterface) throws PcapNativeException {
 
-        PcapHandle packetHandler;
         int snapshotLength = 65536;
         int timeout = 10;
 
-        try {
+        return networkInterface
+                .openLive(snapshotLength, PcapNetworkInterface.PromiscuousMode.PROMISCUOUS, timeout);
+    }
 
-            packetHandler = getInterfaceAny()
-                    .openLive(snapshotLength, PcapNetworkInterface.PromiscuousMode.PROMISCUOUS, timeout);
+    private void receive() throws PcapNativeException, EOFException, TimeoutException, NotOpenException {
 
-            while(packetHandler.getNextPacketEx()!=null){
+        PcapHandle packetHandler = openPacketHandler(Pcaps.getDevByName("any"));
 
+            while(Objects.requireNonNull(packetHandler).getNextPacketEx()!=null){
                     store(packetHandler.getNextPacketEx());
             }
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
